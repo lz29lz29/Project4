efac3a2bc0fb64804f62327ae9ca98dc784111be
lz29lz29 <lz29@njit.edu>
2022-04-22 17:41:18 -0400

    Test the response data of welcome page

--- tests/simple_page_test.py
+++ tests/simple_page_test.py
@@ -5,3 +5,8 @@ def test_request_of_welcome_page(client):
     response = client.get("/welcome")
     assert response.status_code == 200
 
+
+def test_request_example(client):
+    """This makes the index page"""
+    response = client.get("/welcome")
+    assert b"Welcome" in response.data
