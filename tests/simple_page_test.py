8f2101e20a3e1e51484ebda6c006f5dcde8713b5
lz29lz29 <lz29@njit.edu>
2022-04-22 17:51:26 -0400

    Test the response status of about page

--- tests/simple_page_test.py
+++ tests/simple_page_test.py
@@ -10,3 +10,9 @@ def test_request_example(client):
     """This makes the index page"""
     response = client.get("/welcome")
     assert b"Welcome" in response.data
+
+
+def test_request_of_about_page(client):
+    """This makes the index page"""
+    response = client.get("/about")
+    assert response.status_code == 200
