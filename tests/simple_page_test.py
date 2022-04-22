ebb4a123057d7606eec6f85901bfe99f299f3f4a
lz29lz29 <lz29@njit.edu>
2022-04-22 17:58:14 -0400

    Test the response data of about page

--- tests/simple_page_test.py
+++ tests/simple_page_test.py
@@ -16,3 +16,8 @@ def test_request_of_about_page(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
+
+def test_about_page_data(client):
+    """This makes the index page"""
+    response = client.get("/about")
+    assert b"About" in response.data
