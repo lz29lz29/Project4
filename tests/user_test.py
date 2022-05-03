2aae79b677bb6cf636f8651648def139431abfc0
lz29lz29 <lz29@njit.edu>
2022-05-03 19:13:34 -0400

    Add deleting users test

--- tests/user_test.py
+++ tests/user_test.py
@@ -42,3 +42,14 @@ def test_add_transaction(application):
         user1.transactions = [Transaction("200","CREDIT")]
         db.session.commit()
         assert db.session.query(Transaction).count() == 1
+
+
+def test_delete_user(application):
+
+    with application.app_context():
+        user = User('aaa111@google.com', 'zxc123')
+        db.session.add(user)
+        db.session.commit()
+        assert db.session.query(User).count() == 1
+        db.session.delete(user)
+        assert db.session.query(User).count() == 0
