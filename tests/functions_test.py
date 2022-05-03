b91e5330276557b0c79fe0f7c6a13c77a3d8bc6b
lz29lz29 <lz29@njit.edu>
2022-05-03 19:33:31 -0400

    Add login test

--- tests/functions_test.py
+++ tests/functions_test.py
@@ -16,3 +16,24 @@ def test_register(client):
     client.post("/register", data = data)
     user = User.query.filter_by(email='aaa111@test.com').first()
     assert user.email == 'aaa111@test.com'
+
+
+
+def test_login(client):
+    """This makes the index page"""
+    data = {
+        'email': 'aaa111@test.com',
+        'password': 'zzz123',
+        'confirm': 'zzz123'
+    }
+    client.post("/register", data = data)
+    user = User.query.filter_by(email='aaa111@test.com').first()
+    assert user.email == 'aaa111@test.com'
+    data1 = {
+        'email': 'aaa111@test.com',
+        'password': 'zzz123',
+    }
+
+    client.post("/login", data = data1)
+    user = User.query.filter_by(email='aaa111@test.com').first()
+    assert user.authenticated == True
