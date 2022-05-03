15543a149939be2cb52dc18423db1ab43a6c4895
lz29lz29 <lz29@njit.edu>
2022-05-03 16:18:02 -0400

    Add adding user test

--- tests/user_test.py
+++ tests/user_test.py
@@ -12,3 +12,11 @@ def test_user_song_table(application):
         assert db.session.query(User).count() == 0
         assert db.session.query(Transaction).count() == 0
 
+
+def test_adding_user(application):
+
+    with application.app_context():
+        user = User('aaa111@google.com', 'zxc123')
+        db.session.add(user)
+        db.session.commit()
+        assert db.session.query(User).count() == 1
